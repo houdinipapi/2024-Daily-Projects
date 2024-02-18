0bff35542a578463c5d40b976ebae0ff881a4d83
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-18 10:20:35 +0300

    Add URL patterns for user creation, admin, search, and upload

---
+++ Day 13/WebSec/web_sec_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.UserView, name="create_user"),
+    path("admin/", views.admin, name="admin"),
+    path("search/", views.search, name="search"),
+    path("upload/", views.upload, name="upload"),
+]

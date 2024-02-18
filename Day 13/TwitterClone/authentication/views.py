d41095585ea57983f995a2498e6355a212b98d43
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-18 17:05:37 +0300

    Update authentication templates and views

--- Day 13/TwitterClone/authentication/views.py
+++ Day 13/TwitterClone/authentication/views.py
@@ -9,7 +9,6 @@ from .forms import SignUpForm, LoginForm
 def home(request):
     return render(request, "home.html")
 
-@login_required
 def signup(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
@@ -24,3 +23,27 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, "auth/signup.html", {"form": form})
+
+
+def login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                messages.success(request, "Login successful")
+                return redirect("home")
+            else:
+                messages.error(request, "Invalid username or password")
+    else:
+        form = LoginForm()
+    return render(request, "auth/login.html", {"form": form})
+
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect("home")

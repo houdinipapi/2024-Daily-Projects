ce71e0ae32eb2ee2d96e36224287e196321e6bca
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-19 01:41:13 +0300

    Add student CRUD functionality

--- Day 14/CRUD-APP/backend/students/views.py
+++ Day 14/CRUD-APP/backend/students/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import StudentSerializer
+from django.http import JsonResponse
 
 # Create your views here.
+
+class StudentView(APIView):
+    
+    def post(self, request):
+        data = request.data
+        serializer = StudentSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(
+                "Student created successfully",
+                safe=False
+            )
+        
+        return JsonResponse(
+            "Failed to add student",
+            safe=False
+        )

c98b72a711b60b3f7224c4f6482a1f2cf6bbe12e
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-19 01:00:34 +0300

    Add student model and migrations

--- Day 14/CRUD-APP/backend/students/models.py
+++ Day 14/CRUD-APP/backend/students/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    student_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=150)
+    reg_no = models.CharField(max_length=50)
+    course = models.CharField(max_length=150)
+

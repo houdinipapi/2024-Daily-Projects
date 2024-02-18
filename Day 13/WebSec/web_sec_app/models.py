5c390a2463546b08cbd6eb99c166978c3f7226c6
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-18 10:17:41 +0300

    Add User, Search, and UploadFile models

--- Day 13/WebSec/web_sec_app/models.py
+++ Day 13/WebSec/web_sec_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
+    
+
+class Search(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+
+class UploadFile(models.Model):
+    name = models.CharField(max_length=100)
+    file = models.FileField(upload_to="uploads/")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name + " ("+ str(self.file) + ") - " + str(self.uploaded_at)

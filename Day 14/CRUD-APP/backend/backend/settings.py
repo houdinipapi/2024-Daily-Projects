39258080c6ebccd4ca8b48a1f76a17d72494b9fd
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-19 13:52:21 +0300

    Add rest_framework and corsheaders to INSTALLED_APPS

--- Day 14/CRUD-APP/backend/backend/settings.py
+++ Day 14/CRUD-APP/backend/backend/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "students.apps.StudentsConfig",
+    "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
--- Day 14/CRUD-APP/backend/students/views.py
+++ Day 14/CRUD-APP/backend/students/views.py
@@ -1,6 +1,9 @@
 from rest_framework.views import APIView
 from .serializers import StudentSerializer
 from django.http import JsonResponse
+from .models import Student
+from django.http.response import JsonResponse, Http404
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -11,6 +14,22 @@ class StudentView(APIView):
         serializer = StudentSerializer(data=data)
 
         if serializer.is_valid():
+
+            # Check if student already exists
+            existing_student = Student.objects.filter(
+                first_name = data['first_name'],
+                last_name = data['last_name'],
+                email = data['email'],
+                reg_no = data['reg_no'],
+            ).first()
+
+            if existing_student:
+                return JsonResponse(
+                    "Student already exists",
+                    safe=False
+                )
+            
+            # If student doesn't exist, create a new one
             serializer.save()
             return JsonResponse(
                 "Student created successfully",
@@ -21,3 +40,50 @@ class StudentView(APIView):
             "Failed to add student",
             safe=False
         )
+    
+
+    def get_student(self, pk):
+        try:
+            student = Student.objects.get(pk=pk)
+            return student
+        except Student.DoesNotExist:
+            raise Http404
+    
+
+    def get(self, request, pk=None):
+        if pk:
+            data = self.get_student(pk)
+            serializer = StudentSerializer(data)
+        else:
+            data = Student.objects.all()
+            serializer = StudentSerializer(data, many=True)
+            
+        return Response(serializer.data)
+    
+
+    def put(self, request, pk=None):
+        student_to_update = Student.objects.get(student_id=pk)
+        serializer = StudentSerializer(student_to_update, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(
+                "Student updated successfully",
+                safe=False
+            )
+        else:
+            return JsonResponse(
+                "Failed to update student",
+                safe=False
+            )
+
+
+    def delete(self, request, pk):
+        student_to_delete = Student.objects.get(student_id=pk)
+        student_to_delete.delete()
+        return JsonResponse(
+            "Student deleted successfully",
+            safe=False
+        )
+
+

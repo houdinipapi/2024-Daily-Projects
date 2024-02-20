04bcd80eca4935bc75fab7687e407226309ce1ef
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-20 20:13:29 +0300

    Update student data in UpdateStudentModal component

--- Day 14/CRUD-APP/frontend/src/components/UpdateStudentModal.js
+++ Day 14/CRUD-APP/frontend/src/components/UpdateStudentModal.js
@@ -8,13 +8,15 @@ const UpdateStudentModal = (props) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // const formData = new FormData(e.target);
-        // const studentData = Object.fromEntries(formData.entries());
+        const formData = new FormData(e.target);
+        const studentData = Object.fromEntries(formData.entries());
 
-        updateStudent(props.student_id, e.target)
+        console.log("studentData:", studentData);
+
+        updateStudent(studentData, props.student.student_id)
             .then((result) => {
+                console.log("Student updated successfully!");
                 alert(result);
-
                 props.setUpdated(true);
             })
             .catch((error) => {
@@ -45,23 +47,48 @@ const UpdateStudentModal = (props) => {
                             <Form onSubmit={handleSubmit}>
                                 <Form.Group controlId="FirstName">
                                     <Form.Label>First Name</Form.Label>
-                                    <Form.Control type="text" name="first_name" required placeholder="" />
+                                    <Form.Control
+                                        type="text"
+                                        name="first_name"
+                                        required
+                                        defaultValue={props.student.first_name}
+                                        placeholder="" />
                                 </Form.Group>
                                 <Form.Group controlId="LastName">
                                     <Form.Label>Last Name</Form.Label>
-                                    <Form.Control type="text" name="last_name" required placeholder="" />
+                                    <Form.Control
+                                    type="text"
+                                    name="last_name"
+                                    required
+                                    defaultValue={props.student.last_name}
+                                    placeholder="" />
                                 </Form.Group>
                                 <Form.Group controlId="RegistrationNo">
                                     <Form.Label>Registration No.</Form.Label>
-                                    <Form.Control type="text" name="reg_no" required placeholder="" />
+                                    <Form.Control
+                                    type="text"
+                                    name="reg_no"
+                                    required
+                                    defaultValue={props.student.reg_no}
+                                    placeholder="" />
                                 </Form.Group>
                                 <Form.Group controlId="Email">
                                     <Form.Label>Email</Form.Label>
-                                    <Form.Control type="text" name="email" required placeholder="" />
+                                    <Form.Control
+                                    type="text"
+                                    name="email"
+                                    required
+                                    defaultValue={props.student.email}
+                                    placeholder="" />
                                 </Form.Group>
                                 <Form.Group controlId="Course">
                                     <Form.Label>Course</Form.Label>
-                                    <Form.Control type="text" name="course" required placeholder="" />
+                                    <Form.Control
+                                    type="text"
+                                    name="course"
+                                    required
+                                    defaultValue={props.student.course}
+                                    placeholder="" />
                                 </Form.Group>
                                 <Form.Group>
                                     <p></p>

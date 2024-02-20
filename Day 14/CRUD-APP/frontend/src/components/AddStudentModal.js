7186564825192faca246445e102cf17ee90a326b
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-20 16:01:19 +0300

    Add form for adding students to the modal

--- Day 14/CRUD-APP/frontend/src/components/AddStudentModal.js
+++ Day 14/CRUD-APP/frontend/src/components/AddStudentModal.js
@@ -1,24 +1,75 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Col, Row, Form } from "react-bootstrap";
+import { addStudent } from "../services/StudentService";
 
 
 
 const AddStudentModal = (props) => {
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addStudent(e.target)
+        .then((result) => {
+            alert(result);
+        }, (error) => {
+            alert("Failed to Add Student. Please try again.");
+        });
+    };
+
+    
+
     return (
         <div className="container">
-            <Modal {...props}>
+            <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal title</Modal.Title>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Fill In Student Information
+                    </Modal.Title>
                 </Modal.Header>
 
                 <Modal.Body>
-                    <p>Modal body text goes here.</p>
+                    <Row>
+                        <Col sm={6}>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group controlId="FirstName">
+                                    <Form.Label>First Name</Form.Label>
+                                    <Form.Control type="text" name="FirstName" required placeholder="" />
+                                </Form.Group>
+                                <Form.Group controlId="LastName">
+                                    <Form.Label>Last Name</Form.Label>
+                                    <Form.Control type="text" name="LastName" required placeholder="" />
+                                </Form.Group>
+                                <Form.Group controlId="RegistrationNo">
+                                    <Form.Label>Registration No.</Form.Label>
+                                    <Form.Control type="text" name="RegistrationNo" required placeholder="" />
+                                </Form.Group>
+                                <Form.Group controlId="Email">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="text" name="Email" required placeholder="" />
+                                </Form.Group>
+                                <Form.Group controlId="Course">
+                                    <Form.Label>Course</Form.Label>
+                                    <Form.Control type="text" name="Course" required placeholder="" />
+                                </Form.Group>
+                                <Form.Group>
+                                    <p></p>
+                                    <Button variant="primary" type="submit">
+                                        Submit
+                                    </Button>
+                                </Form.Group>
+                            </Form>
+                        </Col>
+                    </Row>
                 </Modal.Body>
-
                 <Modal.Footer>
-                    <Button variant="secondary">Close</Button>
-                    <Button variant="primary">Save changes</Button>
+                    <Button variant="danger" type="submit" onClick={props.onHide}>
+                        Close
+                    </Button>
                 </Modal.Footer>
             </Modal>
         </div>

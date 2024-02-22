e5505d344d66e4ff4769523a7e62ebeda7d43cd2
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-22 12:47:06 +0300

    Add StudentListCreate and StudentDetailView in views.py and update urls.py

--- Day 15/STUDENT-MANAGEMENT-SYSTEM/backend/api/views.py
+++ Day 15/STUDENT-MANAGEMENT-SYSTEM/backend/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from rest_framework import generics
+from .models import Student
+from .serializer import StudentSerializer
+
 # Create your views here.
+
+class StudentListCreate(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class StudentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer

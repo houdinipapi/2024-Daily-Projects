720e7bb51421060cb430e248a25e933a5ae33ca3
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-23 18:23:07 +0300

    Add user registration view with email notification

--- Day 16/USER-AUTHENTICATION/backend/user_accounts/views.py
+++ Day 16/USER-AUTHENTICATION/backend/user_accounts/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from .serializers import UserRegisterSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+class RegisterUserView(GenericAPIView):
+    serializer_class = UserRegisterSerializer
+
+    def post(self, request):
+        user_data = request.data
+        serializer = self.serializer_class(data=user_data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            user = serializer.data
+            # TODO: Send email to user
+
+            return Response({
+                "data": user,
+                "status": "success",
+                "message": f"{user.first_name} registered successfully. Check your email to verify your account"
+            }, status=status.HTTP_201_CREATED)
+        
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+            

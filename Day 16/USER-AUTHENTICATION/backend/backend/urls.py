891781e21e943fa2b29f54e21dbaf34ba3b76822
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-26 16:44:51 +0300

    Add social authentication endpoints

--- Day 16/USER-AUTHENTICATION/backend/backend/urls.py
+++ Day 16/USER-AUTHENTICATION/backend/backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/auth/", include("user_accounts.urls")),
+    path("api/v1/social-auth/", include("social_accounts.urls")),
 ]

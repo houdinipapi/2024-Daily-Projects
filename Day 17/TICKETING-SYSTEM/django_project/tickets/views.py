677396a6abdc431eed5b45747c5034d17968ee8b
Houdini Papi <ochiengcliff.co@gmail.com>
2024-03-09 12:42:07 +0300

    Add create_ticket view for customers

--- Day 17/TICKETING-SYSTEM/django_project/tickets/views.py
+++ Day 17/TICKETING-SYSTEM/django_project/tickets/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import datetime
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Ticket
+from .form import CreateTicketForm, UpdateTicketForm
+
 
 # Create your views here.
+
+"""For customers"""
+
+# Create a ticket
+def create_ticket(request):
+    if request.method == "POST":
+        form = CreateTicketForm(request.POST)
+        if form.is_valid():
+            var = form.save(commit=False)
+            var.created_by = request.user
+            var.ticket_status = "Pending"
+
+            var.save()
+
+            messages.info(request, "Ticket created successfully!")
+            return redirect("dashboard")
+        else:
+            messages.warning(request, "Invalid information!")
+            return redirect("create_ticket")
+    else:
+        form = CreateTicketForm()
+        context = {"form": form}
+
+        return render(request, "tickets/create_ticket.html", context)
+    

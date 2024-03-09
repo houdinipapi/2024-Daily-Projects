1dff6e75387a44b47c89028ca7327db8e54b5b5e
Houdini Papi <ochiengcliff.co@gmail.com>
2024-03-09 19:45:05 +0300

    Add ticket management functionality

--- Day 17/TICKETING-SYSTEM/django_project/tickets/views.py
+++ Day 17/TICKETING-SYSTEM/django_project/tickets/views.py
@@ -31,3 +31,71 @@ def create_ticket(request):
 
         return render(request, "tickets/create_ticket.html", context)
     
+
+# Updating a ticket
+def update_ticket(request, pk):
+    ticket = Ticket.objects.get(pk=pk)
+    form = UpdateTicketForm(instance=ticket)
+
+    if request.method == "POST":
+        form = UpdateTicketForm(request.POST, instance=ticket)
+        if form.is_valid():
+            form.save()
+            messages.info(request, "Ticket updated successfully!")
+            return redirect("dashboard")
+        else:
+            messages.warning(request, "Invalid information!")
+            # return redirect("update_ticket", pk=pk)
+    else:
+        form = UpdateTicketForm(instance=ticket)
+        context = {"form": form}
+        return render(request, "tickets/update_ticket.html", context)
+    
+
+# Viewing all created tickets
+def all_tickets(request):
+    tickets = Ticket.objects.filter(created_by=request.user)
+    context = {
+        "tickets": tickets
+    }
+
+    return render(request, "tickets/all_tickets.html", context)
+
+
+"""For engineers"""
+# View ticket queue
+
+def ticket_queue(request):
+    tickets = Ticket.objects.filter(ticket_status="Pending")
+    context = {
+        "tickets": tickets
+    }
+
+    return render(request, "tickets/ticket_queue.html", context)
+
+
+# Accept a ticket from the queue
+
+def accept_ticket(request, pk):
+    ticket = Ticket.objects.get(pk=pk)
+    ticket.assigned_to = request.user
+    ticket.ticket_status = "In Progress"
+    ticket.accepted_date = datetime.datetime.now()
+    ticket.save()
+
+    messages.info(request, "Ticket accepted successfully!")
+
+    return redirect("ticket_queue")
+
+
+# Close a ticket
+
+def close_ticket(request, pk):
+    ticket = Ticket.objects.get(pk=pk)
+    ticket.ticket_status = "Closed"
+    ticket.closed_date = datetime.datetime.now()
+    ticket.save()
+
+    messages.info(request, "Ticket closed successfully!")
+
+    return redirect("ticket_queue")

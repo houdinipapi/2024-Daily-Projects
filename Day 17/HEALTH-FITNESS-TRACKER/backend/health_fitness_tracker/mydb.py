43deb9c59ead9167d3ea506a5c87d1b9ae750c8d
Houdini Papi <ochiengcliff.co@gmail.com>
2024-03-12 21:57:15 +0300

    Add .env file to .gitignore and create mydb.py for MySQL database connection

---
+++ Day 17/HEALTH-FITNESS-TRACKER/backend/health_fitness_tracker/mydb.py
@@ -0,0 +1,25 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get MySQL credentials from environment variables
+mysql_password = os.getenv("MYSQL_PASSWORD")
+
+
+# Connect to MySQL database
+data_base = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=mysql_password,
+)
+
+cursor_object = data_base.cursor()
+
+cursor_object.execute("CREATE DATBASE IF NOT EXISTS health_fitness_tracker")
+
+cursor_object.execute("USE health_fitness_tracker")
+
+print("Database connected successfully!")
